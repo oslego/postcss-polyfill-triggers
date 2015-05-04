63cd08af2defb8c89bb4f9a91e4c0d964a2c1cbf
Razvan Caliman <razvan.caliman@gmail.com>
2015-05-04 17:29:47 +0100

    generate triggers with CSS animations

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-var postcss = require('postcss');
-var fs = require('fs');
+var postcss   = require('postcss');
+var fs        = require('fs');
+var source    = fs.readFileSync('styles-pre.css');
+var root      = postcss.parse(source);
+var keyframes = {};
+var prefixes  = ['', '-webkit-'];
 
-var source = fs.readFileSync('styles-pre.css');
-var root = postcss.parse(source);
 
-var rules = root.nodes.filter(function(rule){
-  return rule.type === "atrule"
-}).map(function(rule){
-  return {
-    condition: rule.params,
-    rules: rule.nodes.join('').replace(/\n/g, '')
-  }
+var atRuleTemplate = postcss.parse('@keyframes name { to { opacity: 1; } }');
+
+root.eachInside(function (node) {
+    var next         = node.next(),
+        animName     = "",
+        animDuration = "1s",
+        animDecl;
+
+    if (node.type === 'comment' && node.text === '@polyfill' && next.type === 'decl'){
+        animName = next.prop + '\\:' + next.value;
+
+        prefixes.forEach(function (pfx) {
+          animDecl = postcss.decl({ prop: pfx + 'animation', value: animName + ' ' + animDuration });
+          node.parent.insertAfter( next, animDecl);
+        })
+
+        keyframes[animName] = true;
+    }
+});
+
+Object.keys(keyframes).map(function (name) {
+  prefixes.forEach(function (pfx) {
+    var keyframesAtRule = atRuleTemplate.first.clone({ name: pfx + 'keyframes', params: name });
+    root.append(keyframesAtRule);
+  })
 })
 
-var host = postcss.rule({ selector: 'html' })
-host.append({ prop: "content", value: '\'' + JSON.stringify(rules) + '\'' })
-
-root.append(host);
-
-fs.writeFile('styles-post.css', root.toResult().css);
+console.log(root.toString())
